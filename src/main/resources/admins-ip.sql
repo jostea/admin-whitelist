99391f2fb6eb9e0e5a69c9ac9bb958f43b41f2d4
jostea <38920259+jostea@users.noreply.github.com>
2025-01-17 13:33:37 +0200

    Update admins-ip.sql

--- src/main/resources/admins-ip.sql
+++ src/main/resources/admins-ip.sql
@@ -4,7 +4,7 @@ CREATE TABLE admins_ip(
 );
 
 INSERT INTO admins_ip(id, ip)
-VALUES(1, '176.98.31.136');
+VALUES(1, '');
 
 INSERT INTO admins_ip(id, ip)
-VALUES(2, '89.149.109.114');
+VALUES(2, '');
